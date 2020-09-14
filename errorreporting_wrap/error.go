c258ec916af195169fb232bfcb8dd79709672b65
lanches-sawatari <sawatari.toshihiro@lanches.co.jp>
2020-09-14 11:32:37 +0900

    modify error functions

---
+++ errorreporting_wrap/error.go
@@ -0,0 +1,37 @@
+package errorreporting_wrap
+
+import (
+	"cloud.google.com/go/errorreporting"
+	"context"
+	"log"
+)
+
+type Client struct {
+	errorReportingClient *errorreporting.Client
+}
+
+func NewClient(gcpProjectId string) (*Client, error) {
+	erpClient, err := errorreporting.NewClient(context.Background(), gcpProjectId, errorreporting.Config{
+		//ServiceName: serviceName,
+		OnError: func(err error) {
+			log.Printf("Could not log to ErrorReporting, %v", err)
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &Client{errorReportingClient: erpClient}, nil
+}
+
+func (c Client) Log(msg string, err error) {
+	log.Printf("[ERROR] %s: %v", msg, err)
+}
+
+func (c Client) Report(msg string, err error) {
+	c.Log(msg, err)
+	c.errorReportingClient.Report(errorreporting.Entry{Error: err})
+}
+
+func (c Client) ReportWithoutLog(err error) {
+	c.errorReportingClient.Report(errorreporting.Entry{Error: err})
+}
---
+++ errorreporting_wrap/go.mod
@@ -0,0 +1,5 @@
+module github.com/lanches-sawatari/errorreporting_wrap
+
+go 1.13
+
+require cloud.google.com/go v0.65.0
